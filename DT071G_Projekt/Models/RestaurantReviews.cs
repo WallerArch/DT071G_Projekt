769af37dafe16dc36e91811de87efcd2a82c3ffa
Björn Wall <bjawall@hotmail.com>
2023-12-10 12:37:19 +0100

    Skapat modell och controller för recensioner

---
+++ DT071G_Projekt/Models/RestaurantReviews.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SQLite;
+
+namespace DT071G_Projekt.Models
+{
+
+    public static class DatabaseInitializer
+    {
+        public static void Initialize()
+        {
+            // Skapar databasen om den inte finns
+            if (!File.Exists("RestaurantReviews.db"))
+            {
+                SQLiteConnection.CreateFile("RestaurantReviews.db");
+            }
+        }
+    }
+    class RestaurantReview
+    {
+        public int Id { get; set; } // SQLite requires an auto-incrementing integer as the primary key
+        public string Review { get; set; }
+        public string Author { get; set; }
+        public string Restaurant { get; set; }
+    }
+
+}
